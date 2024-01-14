557321cb4090009b09201efa1634ac4e8a4ca84d
Manuel Meister <news.manuelsworld@gmail.com>
2024-01-14 22:18:46 +0100

    Redirect /blog to translated blog

--- src/pages/_redirects.js
+++ src/pages/_redirects.js
@@ -6,6 +6,9 @@ export const get = () => ({
 /closed-beta /en/blog/beta-version 302 Language=en
 /closed-beta /de/blog/beta-version 302 Language=de
 
+/blog /en/blog 302 Language=en
+/blog /de/blog 302 Language=de
+
 /blog/* /en/:splat 302 Language=en
 /blog/* /de/:splat 302 Language=de
 
